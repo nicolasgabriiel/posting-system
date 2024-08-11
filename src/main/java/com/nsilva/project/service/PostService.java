918383e7711b51ba7ba2744642df5163d4fb3da6
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2024-08-11 16:17:32 -0300

    Consulting by query methods

--- src/main/java/com/nsilva/project/service/PostService.java
+++ src/main/java/com/nsilva/project/service/PostService.java
@@ -6,6 +6,7 @@ import com.nsilva.project.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }

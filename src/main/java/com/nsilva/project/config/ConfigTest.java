35ad28f3ad38c2c1f6036c9b57e4be368de5f21a
nicolasgabriiel_ <nicolasgabriel.developer@gmail.com>
2024-08-11 16:05:59 -0300

    List Comments

--- src/main/java/com/nsilva/project/config/ConfigTest.java
+++ src/main/java/com/nsilva/project/config/ConfigTest.java
@@ -1,6 +1,7 @@
 package com.nsilva.project.config;
 
 import com.nsilva.project.dto.AuthorDTO;
+import com.nsilva.project.dto.CommentDTO;
 import com.nsilva.project.entities.Post;
 import com.nsilva.project.entities.User;
 import com.nsilva.project.repository.PostRepository;
@@ -40,6 +41,13 @@ public class ConfigTest implements CommandLineRunner {
 
         Post post1 = new Post(null, LocalDateTime.parse("21/03/2024 22:12:46", sdf), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria) );
         Post post2 = new Post(null, LocalDateTime.parse("23/03/2014 13:31:55", sdf), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria) );;
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", LocalDateTime.parse("21/04/2024 13:30:34", sdf), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", LocalDateTime.parse("22/06/2024 17:11:34",sdf), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", LocalDateTime.parse("23/07/2014 21:40:50", sdf), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
 
         postRepository.saveAll(Arrays.asList(post1, post2));
--- src/main/java/com/nsilva/project/entities/Post.java
+++ src/main/java/com/nsilva/project/entities/Post.java
@@ -1,12 +1,15 @@
 package com.nsilva.project.entities;
 
 import com.nsilva.project.dto.AuthorDTO;
+import com.nsilva.project.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
     private String body;
     private AuthorDTO author;
 
+    private List<CommentDTO> comments = new ArrayList<>();
+
     public Post() {
     }
 
@@ -31,6 +36,15 @@ public class Post implements Serializable {
         this.body = body;
         this.author = author;
     }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
 
     public String getId() {
         return id;
